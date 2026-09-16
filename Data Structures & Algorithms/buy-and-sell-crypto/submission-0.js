b01144792107e140cb7cdf2d31b0f49f9b139998
Camila Andrea <75325895+camilandre@users.noreply.github.com>
2026-09-16 16:41:29 -0500

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let minPrice = prices[0];
+        let maxProfit = 0;
+
+        for (let i = 1; i < prices.length; i++) {
+            const currentProfit = prices[i] - minPrice;
+
+            maxProfit = Math.max(maxProfit, currentProfit);
+            minPrice = Math.min(minPrice, prices[i]);
+        }
+
+        return maxProfit;
+    }
+}
